3eef3853f6ac254782a618f79af1d17d7bbad678
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2024-04-28 19:11:53 -0300

    .

--- src/janelas/lista_dbv.py
+++ src/janelas/lista_dbv.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from src import fontes,images
 import sqlite3
+from CTkTable import *
 
 class ListaDBV(ctk.CTkToplevel):
     def __init__(self,*args,**kwargs):
@@ -10,13 +11,20 @@ class ListaDBV(ctk.CTkToplevel):
         self.columnconfigure(0,weight=1)
         self.columnconfigure(1,weight=2)
 
-        self.cursor_consulta = sqlite3.connect('db/DBS_SYS.db').execute('SELECT * FROM DBV')
+        cursor_listar_dbv = sqlite3.connect('db/DBS_SYS.db').execute('SELECT * FROM DADOS_DBV')
+        cursor_num_rows = sqlite3.connect('db/DBS_SYS.db').execute('SELECT COUNT(0) FROM DBV')
 
         self.img_cadastro_dbv = ctk.CTkButton(master=self,text='',image=images.user(),state='disabled',fg_color='#008000')
         self.lb_titulo = ctk.CTkLabel(master=self,text='Consultar DBV',font=fontes.f_titulos())
         self.fr_formulario = ctk.CTkFrame(master=self,corner_radius=12,fg_color='#282C34')
         self.fr_formulario.columnconfigure((0,1),weight=1)
 
+        value = [i for i in cursor_listar_dbv]
+
+        self.tb_dbv = CTkTable(master=self.fr_formulario, row=int([i for i in cursor_num_rows][0][0]), column=5, values=value,header_color="green",font=fontes.f_campos())
+
         self.img_cadastro_dbv.grid(row=0,column=0,padx=15)
         self.lb_titulo.grid(row=0,column=1,columnspan=2,padx=15,sticky='w')
-        self.fr_formulario.grid(row=1,columnspan=2,padx=15,pady=15)        
+        self.fr_formulario.grid(row=1,columnspan=2,padx=15,pady=15)
+
+        self.tb_dbv.grid(row=0,column=0,columnspan=2,padx=15,pady=15,sticky='nswe')
--- t.py
+++ t.py
@@ -4,10 +4,10 @@ import sqlite3
 
 root = customtkinter.CTk()
 
-cursor_classes = sqlite3.connect('db/DBS_SYS.db').execute('SELECT * FROM DADOS_DBV')
+cursor_listar_dbv = sqlite3.connect('db/DBS_SYS.db').execute('SELECT * FROM DADOS_DBV')
 cursor_num_rows = sqlite3.connect('db/DBS_SYS.db').execute('SELECT COUNT(0) FROM DBV')
 
-value = [i for i in cursor_classes]
+value = [i for i in cursor_listar_dbv]
 
 table = CTkTable(master=root, row=int([i for i in cursor_num_rows][0][0]), column=5, values=value,header_color="green",
                  )
